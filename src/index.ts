e26304232e4765ff4a45f6496ed54c1c06ec1b8e
Christymonthomas <christymonthomas1999@gmail.com>
2024-07-03 15:44:32 +0530

    Added Google Maps

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import { User } from "./User";
-import { Company } from "./Company";
-const user = new User();
-console.log(user);
-const company = new Company();
-console.log(company);
+/// <reference types="@types/google.maps" />
+// import { User } from "./User";
+// import { Company } from "./Company";
+
+const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
